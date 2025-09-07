774683a62d42ebbde41cae95b24a42245f837c95
rayofsunshine <rtao1014@gmail.com>
2025-09-07 10:24:03 -0700

    Enhance Poster component to accept props for dynamic content and add introductory section to main page.

--- src/app/components/Poster.tsx
+++ src/app/components/Poster.tsx
@@ -1,6 +1,12 @@
 import NavigationMenu from "./prototype/NavigationMenu"
 
-export default function Poster() {
+interface PosterProps {
+    name:string;
+    description:string;
+    inspirations:string;
+}
+
+export default function Poster({name, description, inspirations}:PosterProps) {
     return (
         <div className="h-[calc(100vh-20vh)] md:grid grid-cols-8 grid-rows-1 gap-8 md:m-14">
         
@@ -8,11 +14,11 @@ export default function Poster() {
                 <NavigationMenu />
             </div>
             <div className="border-b-1 col-span-3 flex flex-col justify-end gap-1">
-                <h2 className="h-12 border-t-1 text-sm font-mono uppercase tracking-wide">Animated Mini Navigation</h2>
-                <p className="text-sm leading-6">This navigation menu draws inspiration from designs by Microsoft Copilot, Rauno, Vercel. Messing around with motion, shadows, and gradients to add depth and some delight. I utilized Framer Motion to create the effects when the mouse enters or leaves targeted navigation items. #exploration</p>
+                <h2 className="h-12 border-t-1 text-sm font-mono uppercase tracking-wide">{name}</h2>
+                <p className="text-sm leading-6">{description}</p>
                 <div className="border-t-1 flex gap-8">
                 <h2 className="text-sm leading-6">Inspirations</h2>
-                <p className="text-sm leading-6">Rauno</p>
+                <p className="text-sm leading-6">{inspirations}</p>
                 </div>
             </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,11 @@ export default function Home() {
   return (
     <main className="flex flex-col gap-0 m-0 p-0 bg-[#f5f5f5]">
       <Header />
+      <section>
+          <h1 className="h-dvh w-3/4 text-4xl place-items-center">A visual blog documents AI patterns and reflections on things I read and designed.</h1>
+      </section>
       <section className="h-full">
-       <Poster />
-       <Poster />
+       <Poster name="Hello" description="Hello" inspirations="Rauno" />
       </section>
     </main>
   );
